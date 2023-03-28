ba6aa5b5eed8118cbc8564025d4e1120ce75cd9f
Aditi <shrivastvaaditi@gmail.com>
2023-03-28 15:14:06 +0530

    new commit

--- src/pattern/programs/NumericPattern11.java
+++ src/pattern/programs/NumericPattern11.java
@@ -2,9 +2,9 @@ package pattern.programs;
 
 public class NumericPattern11 {
 	public static void main(String[] args) {
-		//int n=5;
-		for(int i=5;i>0;i--) {
-			for(int j=1;j<=i;j++) {
+		// int n=5;
+		for (int i = 5; i > 0; i--) {
+			for (int j = 1; j <= i; j++) {
 				System.out.print(j);
 			}
 			System.out.println();
